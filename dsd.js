cadea3551f879f1f93ab55883e5558e49c0985ae
Alice Pote <alice.writes.wrongs@gmail.com>
2023-09-29 09:41:11 -0400

    implement!

--- dsd.js
+++ dsd.js
@@ -1,4 +1,38 @@
 class DSDElement extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    if (this.shadowRoot) {
+      const spanEl = document.createElement("span");
+      spanEl.textContent = "this content was created in connectedCallback after 'rehydrating'";
+      spanEl.classList.add("hydrated");
+
+      const divEl = document.createElement("div");
+      divEl.appendChild(spanEl);
+      this.shadowRoot.appendChild(divEl)
+
+      const style = document.createElement('style');
+      style.textContent = `:host .hydrated {
+        color: red;
+      }`;
+      this.shadowRoot.append(style);
+    } else {
+      this.attachShadow({ mode: "open" });
+
+      const spanEl = document.createElement("span");
+      spanEl.textContent = "this content was created in connectedCallback from scratch!";
+      this.shadowRoot.appendChild(spanEl);
+
+      const style = document.createElement('style');
+      style.textContent = `span {
+        color: pink;
+      }`;
+      this.shadowRoot.append(style);
+
+    }
+  }
 }
 
 window.customElements.define("dsd-element", DSDElement);
